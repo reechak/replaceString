44c4434272992d95371c3fba721f24da5f3204c8
Reecha Kansal <r.kansal9@gmail.com>
2018-09-09 19:46:08 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            int number;
+            bool isNumeric = int.TryParse(line, out number);
+            var newLine = "";
+
+            if (!isNumeric)
+            {
+                newLine = replaceString(line);
+                Console.WriteLine(newLine);
+            }
+
+        }
+    }
+
+    public static string replaceString(string input)
+    {
+        return Regex.Replace(input, @"\s+", "%20");
+
+    }
+
+
+
+}
